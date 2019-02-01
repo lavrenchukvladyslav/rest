23693fa30119cca34bd50df346b7cf765896ca9e
VladyslavLavrenchuk <lavrenchukvladyslav@gmail.com>
2019-02-01 17:42:55 +0200

    rest

---
+++ models/Country.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace app\models;
+
+use yii\db\ActiveRecord;
+
+class Country extends ActiveRecord
+{
+
+}
+//namespace app\controllers;
+//
+//use yii\rest\ActiveController;
+//
+//class UserController extends ActiveController
+//{
+//    public $modelClass = 'app\models\Country';
+//}
